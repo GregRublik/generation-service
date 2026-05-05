055012ed56bc9e63f93b3aabd48176e7b9d6ed16
Админ <@PC-1028006.Scent>
2026-05-05 15:38:57 +0300

    connect roter generator

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
 
-from api.v1.endpoints import prompts
+from api.v1.endpoints import prompts, responses
 from config import settings
 
 app = FastAPI()
 
 app.include_router(prompts.router)
+app.include_router(responses.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host=settings.host, port=settings.port)
